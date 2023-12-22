4d52031e34c7dd1d406e714300095d7f571eb21e
June Lee <junelee@Junes-Laptop.local>
2023-12-22 13:45:36 -0600

    added some console.logs

--- hello.js
+++ hello.js
@@ -4,7 +4,10 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Wsup, World");
+console.log(2+2);
+console.log("wsup","\t","back");
+console.log('"quotations"');
 
 /*  
 Next, PLAY! 
